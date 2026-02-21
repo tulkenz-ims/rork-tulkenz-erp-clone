c21df32e7629935d266c06a005c853050c8de6ac
Virginia Kessler <virginia.kessler@tulkenz.net>
2026-02-21 11:46:08 -0600

    Delete app/auditorportal directory

--- app/auditorportal/audit.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function TestPortal() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
-      <Text style={{ fontSize: 24 }}>Portal Route Works</Text>
-    </View>
-  );
-}
