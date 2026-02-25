870d0efec7913a1bb7f3498e9359f75c6ea3f766
Virginia Kessler <virginia.kessler@tulkenz.net>
2026-02-25 04:54:18 -0600

    Update taskFeedTemplates.ts

--- types/taskFeedTemplates.ts
+++ types/taskFeedTemplates.ts
@@ -24,6 +24,7 @@ export interface FormField {
   defaultValue?: string | string[] | boolean;
   helpText?: string;
   sortOrder: number;
+  dynamicSource?: string;
 }
 
 export interface WorkflowRule {
